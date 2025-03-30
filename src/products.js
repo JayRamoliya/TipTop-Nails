384a5a01302cdc1b85b91dc01d028d5ccca8fe2a
Jay <ramoliyajay9@gmail.com>
2025-03-30 22:25:48 +0530

    add products

--- src/products.js
+++ src/products.js
@@ -6,9 +6,11 @@ const products = [
     { id: 5, name: "Ombre Pink Nails", category: "4", price: "$2.5", image: "/nails/Ombre Pink Nails.jpg", link: "https://amzn.to/3Rtscve" },
     { id: 6, name: "Acrylic Nails", category: "4", price: "$2.5", image: "/nails/Acrylic Nails.jpg", link: "https://amzn.to/3Rs0PSh" },
     { id: 7, name: "French Nails", category: "4", price: "$2.5", image: "/nails/French Nails.jpg", link: "https://amzn.to/3XDWMG7" },
-    { id: 8, name: "French Nails", category: "4", price: "$2.5", image: "/nails/French Nails.jpg", link: "https://amzn.to/4hYqFIC" },
+    { id: 8, name: "Matte Green Shadeand", category: "4", price: "$2.5", image: "/nails/matte green shadeand.jpg", link: "https://amzn.to/4hYqFIC" },
     { id: 9, name: "Gel Nails", category: "4", price: "$2.5", image: "/nails/Gel Nails.jpg", link: "https://amzn.to/4hTWnGP" },
     { id: 10, name: "Ombre Nails", category: "4", price: "$2.5", image: "/nails/Ombre Nails.jpg", link: "https://amzn.to/4hV9dEG" },
+    { id: 11, name: "Dark Emerald Green Nail Designs", category: "1", price: "$3.5", image: "/nails/Dark Emerald Green Nail Designs.jpg", link: "https://amzn.to/3DYkoi5" },
+    { id: 12, name: "Gorgeous Green and Gold Nails", category: "2", price: "$3.1", image: "/nails/Gorgeous Green and Gold Nails.jpg", link: "https://amzn.to/4iNEKdd" },
 ];
 
 export default products;
